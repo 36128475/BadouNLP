f87807e3607588a5dd6c9095dc9b20cfd473ad0b
36128475 <guanjianjia@163.com>
2025-01-23 20:33:50 +0800

    Update work.py

--- 关健佳/week02/work.py
+++ 关健佳/week02/work.py
@@ -1,1 +1,15 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+class MyMoudle(nn.Moudle):
+  def __init__(self, inputs, hidden1, hidden2, outputs):
+    super(self, MyMoudle).__init__()
+    self.layer1 = nn.Linear(inputs, hidden1)
+    self.layer2 = nn.Linear(hidden1,hidden2)
+
+  def forward(self, x):
+    x = self.layer1(x)
+    x = self.layer2(x)
+    return nn.Softmax(x)
 
