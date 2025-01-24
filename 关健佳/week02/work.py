78a2773d461a1c44a925b1a278f8e499f22eb49b
36128475 <guanjianjia@163.com>
2025-01-24 17:23:16 +0800

    Update work.py

--- 关健佳/week02/work.py
+++ 关健佳/week02/work.py
@@ -3,13 +3,24 @@ import torch.nn as nn
 import numpy as np
 
 class MyMoudle(nn.Moudle):
-  def __init__(self, inputs, hidden1, hidden2, outputs):
+  def __init__(self, inputs):
     super(self, MyMoudle).__init__()
-    self.layer1 = nn.Linear(inputs, hidden1)
-    self.layer2 = nn.Linear(hidden1,hidden2)
+    
 
   def forward(self, x):
     x = self.layer1(x)
     x = self.layer2(x)
     return nn.Softmax(x)
 
+
+
+def train():
+  #超参数
+  epoch_num = 100
+  batch_size = 24
+  train_sample = 5000
+  inputs_size = 5
+  learning_rate = 0.01
+
+  model = MyMoudle(inputs_size)
+  
